30e941baf20df95353b32f6bd5eb248668432f71
Pascal Grimaud <pascalgrimaud@gmail.com>
2015-12-21 22:40:44 +0100

    Reduce sleep time for starting containers

--- travis/scripts/04-docker.sh
+++ travis/scripts/04-docker.sh
@@ -9,10 +9,10 @@ if [ -a docker-compose-prod.yml ]; then
   echo '    privileged: true' >> docker-compose-prod.yml
   docker-compose -f docker-compose-prod.yml up -d
   if [ $JHIPSTER == "app-cassandra" ]; then
-    sleep 30
+    sleep 20
     docker exec -it samplecassandra-cassandra init
   else
-    sleep 20
+    sleep 15
   fi
   docker ps -a
 fi
