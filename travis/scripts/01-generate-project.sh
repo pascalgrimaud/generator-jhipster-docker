1a9d4a087c24919e82535ce0157968299090c57e
Pascal Grimaud <pascalgrimaud@gmail.com>
2015-12-26 00:09:04 +0100

    Travis : display the docker-compose files

--- travis/scripts/01-generate-project.sh
+++ travis/scripts/01-generate-project.sh
@@ -16,3 +16,12 @@ yo jhipster-docker default --force --no-insight
 ls -al $HOME/$JHIPSTER
 ls -al $HOME/$JHIPSTER/node_modules/generator-jhipster/
 ls -al $HOME/$JHIPSTER/node_modules/generator-jhipster/entity/
+#-------------------------------------------------------------------------------
+# Display the docker-compose files
+#-------------------------------------------------------------------------------
+if [ -a docker-compose.yml ]; then
+  cat docker-compose.yml
+fi
+if [ -a docker-compose-prod.yml ]; then
+  cat docker-compose-prod.yml
+fi
